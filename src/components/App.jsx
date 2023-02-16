45fef87e7337808d38bce10b577e507cb247bff0
Oleksii Skladaniuk <93865262+Skladaniuk@users.noreply.github.com>
2023-02-16 21:37:37 +0200

    commit#2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from '../redux/store';
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+  const value = useSelector(state => state.myValue)
+  const dispatch = useDispatch()
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+     {value}
+     <button onClick = { () => dispatch(increment(1))}>Increment</button>
+     <button onClick={ () => dispatch(decrement(1))}>Decrement</button>
+      </div>
+    );
+  };
---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
+
+
+export const increment = createAction('myValue/increment');
+export const decrement = createAction('myValue/decrement');
+console.log(increment())
+
+const myReducer = createReducer(10, {
+  [increment]: (state, action) => state + action.payload,
+  [decrement]:(state, action) => state - action.payload,
+})
+
+export const store = configureStore({
+  reducer: {
+    myValue: myReducer,
+  },
+})
